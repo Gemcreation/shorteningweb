a497e6dec4983019b54abddd048697df33b62dd1
Gem <mojeedibrahim13@gmail.com>
2025-11-24 06:56:16 -0800

    done with all componet and style for the desktop

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,8 +3,46 @@
 const Footer = () => {
 
     return(
-        <>
-        </>
+        <footer className="footer">
+          <div className="cta">
+            <h2>Boost your links today</h2>
+            <button>Get Started</button>
+          </div>
+    
+          <div className="footer-links">
+            <div className="brand">Shortly</div>
+    
+            <div className="links">
+              <div>
+                <h4>Features</h4>
+                <ul>
+                  <li>Link Shortening</li>
+                  <li>Branded Links</li>
+                  <li>Analytics</li>
+                </ul>
+              </div>
+    
+              <div>
+                <h4>Resources</h4>
+                <ul>
+                  <li>Blog</li>
+                  <li>Developers</li>
+                  <li>Support</li>
+                </ul>
+              </div>
+    
+              <div>
+                <h4>Company</h4>
+                <ul>
+                  <li>About</li>
+                  <li>Our Team</li>
+                  <li>Careers</li>
+                  <li>Contact</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </footer>
     );
 }
 
