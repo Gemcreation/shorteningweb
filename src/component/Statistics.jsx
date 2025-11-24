69de89cceaf148a9e6d5e8ec9ebcbb7c71d5b2f4
Gem <mojeedibrahim13@gmail.com>
2025-11-24 12:36:20 -0800

    pushing

--- src/component/Statistics.jsx
+++ src/component/Statistics.jsx
@@ -4,22 +4,22 @@ const Statistics = () => {
     const stats = [
       {
         title: "Brand Recognition",
-        text: "Boost your brand recognition with each click. Branded links help instill confidence in your content.",
+        text: "Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.",
       },
       {
         title: "Detailed Records",
-        text: "Gain insights into who is clicking your links. Knowing when and where people engage helps inform decisions.",
+        text: "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.",
       },
       {
         title: "Fully Customizable",
-        text: "Improve brand awareness and content discoverability through customizable links.",
+        text: "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.",
       },
     ];
 
     return(
         <section className="statistics">
           <h2>Advanced Statistics</h2>
-          <p>Track how your links are performing across the web.</p>
+          <p>Track how your links are performing across the web with<br /> our advanced statistics dashboard.</p>
     
           <div className="cards">
             {stats.map((stat, i) => (
