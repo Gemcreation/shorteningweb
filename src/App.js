1152cebbc79d6315dae917eced6a273bd0f70be6
Gem <mojeedibrahim13@gmail.com>
2025-11-24 03:17:59 -0800

    all neccassary componet added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Header from "./component/Header";
+import HeroSection from "./component/HeroSection";
+import ShortenLink from "./component/ShortenLink";
+import Statistics from "./component/Statistics";
+import Footer from "./component/Footer";
 import './App.scss';
 
 function App() {
   return (
     <>
       <Header/>
+      <HeroSection/>
+      <ShortenLink/>
+      <Statistics/>
+      <Footer/>
     </>
   );
 }
