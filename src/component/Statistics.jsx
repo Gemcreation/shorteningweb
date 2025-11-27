2ccecc07d0af79674d9eaf2b7f9c6c8db8c5f5d7
Gem <mojeedibrahim13@gmail.com>
2025-11-26 18:16:01 -0800

    add

--- src/component/Statistics.jsx
+++ src/component/Statistics.jsx
@@ -33,15 +33,15 @@ const Statistics = () => {
           </div> */}
           {/*  */}
           <div className="cards">
-            <div className="card" >
+            <div className="card1" >
               <h3>Brand Recognition</h3>
               <p>Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.</p>
             </div>
-            <div className="card" >
+            <div className="card2" >
               <h3>Detailed Records</h3>
               <p>Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.</p>
             </div>
-            <div className="card" >
+            <div className="card3" >
               <h3>Fully Customizable</h3>
               <p>Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.</p>
             </div>
