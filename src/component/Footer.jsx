37d0ee29f8d9ee095ca3fac2bd4970ada78bc46e
Gem <mojeedibrahim13@gmail.com>
2026-01-09 20:01:35 -0800

    footer side added

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,4 +1,4 @@
-
+import React from "react";
 import headerLogo from "../assets/images/logo.svg";
 import facebook from "../assets/images/icon-facebook.svg";
 import twitter from "../assets/images/icon-twitter.svg";
@@ -6,65 +6,113 @@ import pinterest from "../assets/images/icon-pinterest.svg";
 import instagram from "../assets/images/icon-instagram.svg";
 
 const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-links">
+        <div className="brand">
+          <a href="/">
+            <img src={headerLogo} className="site-header" alt="Shortly logo" />
+          </a>
+        </div>
 
-    return(
-        <footer className="footer">
-          <div className="footer-links">
-                <div className="brand">
-                    <a href="/">
-                        <img src={headerLogo} className="site-header" alt="Room logo" />
-                    </a>
-                </div>
-    
-            <div className="links">
-              <div>
-                <h4>Features</h4>
-                <ul>
-                  <li>Link Shortening</li>
-                  <li>Branded Links</li>
-                  <li>Analytics</li>
-                </ul>
-              </div>
-    
-              <div>
-                <h4>Resources</h4>
-                <ul>
-                  <li>Blog</li>
-                  <li>Developers</li>
-                  <li>Support</li>
-                </ul>
-              </div>
-    
-              <div>
-                <h4>Company</h4>
-                <ul>
-                  <li>About</li>
-                  <li>Our Team</li>
-                  <li>Careers</li>
-                  <li>Contact</li>
-                </ul>
-              </div>
-              {/* socail media handles */}
-              <div>
-                <ul>
-                  <li>
-                    <a href=""><img src={facebook} alt="facebook" /></a>
-                  </li>
-                  <li>
-                    <a href=""><img src={twitter} alt="twitter" /></a>
-                  </li>
-                  <li>
-                    <a href=""><img src={pinterest} alt="pinterest" /></a>
-                  </li>
-                  <li>
-                    <a href=""><img src={instagram} alt="instagram" /></a>
-                  </li>
-                </ul>
-              </div>
-            </div>
+        <div className="links">
+          <div>
+            <h4>Features</h4>
+            <ul>
+              <li>
+                <a href="#features">Link Shortening</a>
+              </li>
+              <li>
+                <a href="#features">Branded Links</a>
+              </li>
+              <li>
+                <a href="#features">Analytics</a>
+              </li>
+            </ul>
           </div>
-        </footer>
-    );
-}
+
+          <div>
+            <h4>Resources</h4>
+            <ul>
+              <li>
+                <a href="#resources">Blog</a>
+              </li>
+              <li>
+                <a href="#resources">Developers</a>
+              </li>
+              <li>
+                <a href="#resources">Support</a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h4>Company</h4>
+            <ul>
+              <li>
+                <a href="#company">About</a>
+              </li>
+              <li>
+                <a href="#company">Our Team</a>
+              </li>
+              <li>
+                <a href="#company">Careers</a>
+              </li>
+              <li>
+                <a href="#company">Contact</a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Social media handles */}
+          <div>
+            <ul>
+              <li>
+                <a
+                  href="https://facebook.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Facebook"
+                >
+                  <img src={facebook} alt="Facebook" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://twitter.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Twitter"
+                >
+                  <img src={twitter} alt="Twitter" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://pinterest.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Pinterest"
+                >
+                  <img src={pinterest} alt="Pinterest" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://instagram.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Instagram"
+                >
+                  <img src={instagram} alt="Instagram" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -1,24 +1,24 @@
-
+import React from "react";
 import heroImg from "../assets/images/illustration-working.svg";
 
 const HeroSection = () => {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1>More than just shorter links</h1>
+        <p>
+          Build your brand's recognition and get detailed insights on how your
+          links are performing.
+        </p>
+        <button>Get Started</button>
+      </div>
 
-    return(
-        <section className="hero">
-          <div className="hero-content">
-            <h1>More than just shorter links</h1>
-            <p>Build your brand’s recognition and get detailed <br></br> insights on how your links are performing.
-            </p>
-            <button>Get Started</button>
-          </div>
-    
-          {/* Illustration on the right */}
-          <div className="hero-image">
-            <img src={heroImg} alt="Working Illustration" />
-          </div>
-          {/* I dont later make use of this hero image because it's not working well for what i need to fo. So instead i use it as a backgroung image on and it works fine for me */}
-        </section>
-    );
-}
+      {/* Illustration on the right */}
+      <div className="hero-image">
+        <img src={heroImg} alt="Working Illustration" />
+      </div>
+    </section>
+  );
+};
 
 export default HeroSection;
